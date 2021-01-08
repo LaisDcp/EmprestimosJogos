dd17f427140aeaba91916cb541f44a8af38f5447
Laís de Castro <lais_dcp@hotmail.com>
2021-01-08 00:02:49 -0300

    FluentValidations para as ViewModels.

---
+++ src/EmprestimosJogos.Application/Validations/AmigoValidation.cs
@@ -0,0 +1,39 @@
+﻿using EmprestimosJogos.Application.ViewModels;
+using EmprestimosJogos.Domain.Core.Enum;
+using EmprestimosJogos.Infra.CrossCutting.Helpers;
+using FluentValidation;
+
+namespace EmprestimosJogos.Application.Validations
+{
+    public class AmigoValidation : AbstractValidator<AmigoViewModel>
+    {
+        private string _campoObrigatorioMessage = ApiErrorCodes.CAMPOBRG.GetDescription();
+        public AmigoValidation()
+        {
+            RuleFor(u => u.PerfilId)
+               .NotNull()
+                   .WithMessage(_campoObrigatorioMessage);
+
+            RuleFor(u => u.Nome)
+                .NotNull()
+                    .WithMessage(_campoObrigatorioMessage)
+                .NotEmpty()
+                    .WithMessage(_campoObrigatorioMessage);
+
+            RuleFor(u => u.CEP)
+                .MinimumLength(8)
+                .When(u => !string.IsNullOrEmpty(u.CEP))
+                    .WithMessage(ApiErrorCodes.INVCEP.GetDescription());
+
+            RuleFor(u => u.TelefoneFixo)
+                .MinimumLength(10)
+                .When(u => !string.IsNullOrEmpty(u.TelefoneFixo))
+                    .WithMessage(ApiErrorCodes.INVTEL.GetDescription());
+
+            RuleFor(u => u.TelefoneCelular)
+                .MinimumLength(10)
+                .When(u => !string.IsNullOrEmpty(u.TelefoneCelular))
+                    .WithMessage(ApiErrorCodes.INVCEL.GetDescription());
+        }
+    }
+}
---
+++ src/EmprestimosJogos.Application/Validations/UsuarioValidation.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using EmprestimosJogos.Domain.Core.Enum;
+using EmprestimosJogos.Infra.CrossCutting.Helpers;
+using EmprestimosJogos.Application.ViewModels;
+
+namespace EmprestimosJogos.Application.Validations
+{
+    public class UsuarioValidation : AbstractValidator<UsuarioViewModel>
+    {
+        private string _campoObrigatorioMessage = ApiErrorCodes.CAMPOBRG.GetDescription();
+
+        public UsuarioValidation()
+        {
+            RuleFor(u => u.Email)
+               .NotNull()
+                   .WithMessage(_campoObrigatorioMessage)
+               .NotEmpty()
+                   .WithMessage(_campoObrigatorioMessage);
+
+            RuleFor(u => u.Senha)
+                .NotNull()
+                    .WithMessage(_campoObrigatorioMessage)
+                .NotEmpty()
+                   .WithMessage(_campoObrigatorioMessage);
+        }
+    }
+}
