a2e8800fb6c89c067f7153fc9b8dc51ffc19b639
Laís de Castro <lais_dcp@hotmail.com>
2021-01-08 00:05:38 -0300

    Arquitetura para testes na camada de aplicação.

---
+++ src/EmprestimosJogos.Services.ApiTests/IntegrationTest/UsuarioTests.cs
@@ -0,0 +1,19 @@
+﻿using EmprestimosJogos.Services.Api;
+using EmprestimosJogos.Services.ApiTests.Config;
+using Xunit;
+
+namespace EmprestimosJogos.Services.ApiTests.IntegrationTest
+{
+    [Collection(nameof(IntegrationApiTestsFixtureCollection))]
+    public class UsuarioTests
+    {
+        private readonly IntegrationTestsFixture<StartupTests> _testsFixture;
+        private string _baseEndpoint = "/api/v1/usuarios";
+
+        public UsuarioTests(IntegrationTestsFixture<StartupTests> testsFixture)
+        {
+            _testsFixture = testsFixture;
+        }
+
+    }
+}
