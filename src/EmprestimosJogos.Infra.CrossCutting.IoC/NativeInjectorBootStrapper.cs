10d9b12be9677f6e3e8ff5d0eebdc07131619060
Laís de Castro <lais_dcp@hotmail.com>
2021-01-08 00:04:11 -0300

    Injeção de Dependência no IoC.

--- src/EmprestimosJogos.Infra.CrossCutting.IoC/NativeInjectorBootStrapper.cs
+++ src/EmprestimosJogos.Infra.CrossCutting.IoC/NativeInjectorBootStrapper.cs
@@ -1,9 +1,16 @@
-﻿using EmprestimosJogos.Domain.Interfaces.Facades;
+﻿using EmprestimosJogos.Application.Interfaces;
+using EmprestimosJogos.Application.Services;
+using EmprestimosJogos.Domain.Entities;
+using EmprestimosJogos.Domain.Interfaces.Facades;
+using EmprestimosJogos.Domain.Interfaces.Repositories;
 using EmprestimosJogos.Domain.Interfaces.UoW;
+using EmprestimosJogos.Domain.Validations;
 using EmprestimosJogos.Infra.CrossCutting.Auth.Facades;
 using EmprestimosJogos.Infra.Data.Context;
+using EmprestimosJogos.Infra.Data.Repositories;
 using EmprestimosJogos.Infra.Data.UoW;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EmprestimosJogos.Infra.CrossCutting.IoC
@@ -15,6 +22,8 @@ namespace EmprestimosJogos.Infra.CrossCutting.IoC
             services.AddDbContext<EmprestimosJogosContext>();
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            services.AddScoped<IPasswordValidator<Usuario>, PasswordValidation>();
 
             #region Facades
             services.AddSingleton<IAuthFacade, AuthFacade>();
@@ -28,10 +37,17 @@ namespace EmprestimosJogos.Infra.CrossCutting.IoC
 
             #region AppServices
 
+            services.AddScoped<IAmigoAppService, AmigoAppService>();
+            services.AddScoped<IUsuarioAppService, UsuarioAppService>();
             #endregion
 
             #region Repositories
 
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+            services.AddScoped<IPerfilRepository, PerfilRepository>();
+            services.AddScoped<IAmigoRepository, AmigoRepository>();
+            services.AddScoped<ITokenRepository, TokenRepository>();
+            services.AddScoped<ITokenTypeRepository, TokenTypeRepository>();
             #endregion
         }
     }
